9ac0eb227b4bd1851424a83d9b5913e034eec25f
Debajyoti Dutta <debajyotidutta.50@gmail.com>
2026-05-31 09:54:57 +0530

    server and index files updated

--- src/index.js
+++ src/index.js
@@ -1,12 +1,57 @@
-const { startServer } = require("./server/server");
+const logger = require("./config/logger");
 
-let server;
+const {
+  startServer,
+  stopServer,
+} = require("./server/server");
 
-(async () => {
+async function bootstrap() {
   try {
-    server = await startServer();
+    await startServer();
   } catch (error) {
+    logger.fatal(error);
     process.exit(1);
-    console.error(error);
   }
-})();
+}
+
+async function shutdown(signal) {
+  logger.info(`${signal} received`);
+
+  try {
+    await stopServer();
+    process.exit(0);
+  } catch (error) {
+    logger.error(error);
+    process.exit(1);
+  }
+}
+
+process.on("SIGINT", () => {
+  shutdown("SIGINT");
+});
+
+process.on("SIGTERM", () => {
+  shutdown("SIGTERM");
+});
+
+process.on("uncaughtException", async (error) => {
+  logger.fatal(error);
+
+  try {
+    await stopServer();
+  } finally {
+    process.exit(1);
+  }
+});
+
+process.on("unhandledRejection", async (reason) => {
+  logger.fatal(reason);
+
+  try {
+    await stopServer();
+  } finally {
+    process.exit(1);
+  }
+});
+
+bootstrap();
--- src/server/server.js
+++ src/server/server.js
@@ -1,15 +1,58 @@
 const http = require("http");
+
 const app = require("./app");
 const logger = require("../config/logger");
+const env = require("../config/env");
+
+const {
+  initializeDatabase,
+  closeDatabase,
+} = require("../config/database");
+
+const {
+  initializeRedis,
+  closeRedis,
+} = require("../config/redis");
+
+let server;
 
 async function startServer() {
-  //DB init
-  const server = http.createServer(app);
-  server.listen(8000, () => {
-    logger.info("Server started on port 3000");
+  await initializeDatabase();
+  await initializeRedis();
+
+  server = http.createServer(app);
+
+  server.listen(env.app.port, () => {
+    logger.info(
+      `Server started on port ${env.app.port}`
+    );
   });
 
   return server;
 }
 
-module.exports = {startServer};
+async function stopServer() {
+  logger.info("Stopping server...");
+
+  if (server) {
+    await new Promise((resolve, reject) => {
+      server.close((error) => {
+        if (error) {
+          return reject(error);
+        }
+
+        resolve();
+      });
+    });
+  }
+
+  await closeRedis();
+  await closeDatabase();
+
+  logger.info("Server stopped");
+}
+
+module.exports = {
+  startServer,
+  stopServer,
+};
